4e3c5b8ece7c14e4569b04db5ed07d0334d0b6b3
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-04-29 02:50:08 +0000

    応用課題7b　完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,12 @@ class Book < ApplicationRecord
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } #前日
+  
+  scope :created_this_week, -> { where(created_at: Time.zone.now.all_week) }
+  scope :created_last_week, -> { where(created_at: 1.week.ago.all_week) }
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
